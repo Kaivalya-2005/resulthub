81bb81b655da2eaa2bae449a0a6ea00069ca7db6
Kaivalya Kuldip Deshpande <kaivalya.deshpande2005@gmail.com>
2025-05-01 18:40:05 +0530

    hii

--- frontend/src/components/results/ToppersList.tsx
+++ frontend/src/components/results/ToppersList.tsx
@@ -52,7 +52,7 @@ const ToppersList = ({ toppers }: { toppers: Topper[] }) => {
                   </td>
                   <td className="px-6 py-4 whitespace-nowrap">
                     <div className="text-sm font-bold text-gray-900">
-                      {student.percentage.toFixed(2)}%
+                      {student.percentage}%
                     </div>
                   </td>
                 </tr>
