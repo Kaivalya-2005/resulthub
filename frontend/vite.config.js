586e35838e4d4ac8508255d5f81375cb43a1e9ab
Kaivalya Kuldip Deshpande <kaivalya.deshpande2005@gmail.com>
2025-05-03 20:25:26 +0530

    u

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { string } from 'vite-plugin-string'; // ✅ Import plugin
+import string from 'vite-plugin-string'; // ✅ Import plugin
 
 export default defineConfig({
   plugins: [
