8430e1d189e1c4bcc794fc0ba8874b1e5a49987c
Kaivalya Kuldip Deshpande <kaivalya.deshpande2005@gmail.com>
2025-05-13 12:48:53 +0530

    f

--- backend/utils/fetchResults.js
+++ backend/utils/fetchResults.js
@@ -27,13 +27,13 @@ const startPollingUntilSuccess = async () => {
 };
 
 const fetchResultsFromSite = async () => {
-  const url = 'https://mahresult.nic.in/sscmarch2024/sscresultviewmarch24.asp';
+  const url = 'https://mahresult.nic.in/sscmarch2025/sscresultviewmarch25.asp';
 
   const headers = {
     "User-Agent": "Mozilla/5.0",
     "Content-Type": "application/x-www-form-urlencoded",
     "Origin": "https://mahresult.nic.in",
-    "Referer": "https://mahresult.nic.in/sscmarch2024/sscmarch2024.htm"
+    "Referer": "https://mahresult.nic.in/sscmarch2025/sscmarch2025.htm"
   };
 
    // Only fetch students whose total_marks is null or 0
